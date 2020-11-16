d940913bfd552e2bd9d51f49ade5d92aaf9f5b70
Thaleszh <thaleszh@gmail.com>
2020-11-16 16:50:04 -0300

    Fresh start

---
+++ trades/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from trades.models import Deal, Pair, Provider
+# Register your models here.
+
+@admin.register(Deal)
+class DealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pair', 'quantity')
+    search_fields = ['pair', 'quantity']
+
+
+@admin.register(Pair)
+class PairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'time', 'provider')
+    search_fields = ['name', 'time', 'provider', 'time']
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ['name']
