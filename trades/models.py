d2bee7f776b7de69062b051ff6a0bc37e871e164
Thaleszh <thaleszh@gmail.com>
2020-11-16 16:50:16 -0300

    Base DB schema

---
+++ trades/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+# Create your models here.
+class Provider(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField (
+        max_length=64
+    )
+
+
+class User(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(
+        max_length=64
+    )
+
+
+class Pair(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(
+        max_length=64
+    )
+
+    time = models.DateTimeField
+
+    price = models.FloatField(
+        max_length=32
+    )
+
+    provider = models.ForeignKey(
+        to=Provider,
+        on_delete=models.CASCADE
+    )
+
+
+class Deal(models.Model):
+    def __str__(self):
+        return self.pair + 'x' + self.quantity
+
+    quantity = models.ImageField(
+        max_length=32
+    )
+
+    pair = models.ForeignKey(
+        to=Pair,
+        on_delete=models.CASCADE
+    )
