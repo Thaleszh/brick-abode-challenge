036bbaeaeb6ca261df03bf141d49a1894a626693
Thaleszh <thaleszh@gmail.com>
2020-11-16 17:19:24 -0300

    Implemented simple viewsets

--- trades/views.py
+++ trades/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
+from trades.models import Deal, Pair, Provider
+import trades.serializers as serializer
 # Create your views here.
+
+
+class DealViewSet(ModelViewSet):
+    queryset = Deal.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return serializer.ReadDealSerializer
+        return serializer.CreateDealSerializer
+
+
+class PairViewSet(ModelViewSet):
+    queryset = Pair.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return serializer.ReadPairSerializer
+        return serializer.CreatePairSerializer
+
+
+class ProviderViewSet(ModelViewSet):
+    queryset = Provider.objects.all()
